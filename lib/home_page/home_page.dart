1b58a690745e2792e168782d76c5b917f1f74e28
Jaorobert <jaorobert125@gmail.com>
2024-03-10 14:53:35 -0300

    criado tela de login e arrumado algumas coisas

---
+++ lib/home_page/home_page.dart
@@ -0,0 +1,107 @@
+import 'dart:ui';
+
+import 'package:app_facul/login_page/Login.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/imgs/logo-unicv.png'),
+            fit: BoxFit.contain,
+            opacity: 0.5),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const Text(
+            "UniCV Recados",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                color: Color.fromRGBO(72, 92, 57, 30),
+                decoration: TextDecoration.none),
+          ),
+          SizedBox(
+            width: 100,
+          ),
+          Center(
+              child: Container(
+                  padding: const EdgeInsets.all(2),
+                  alignment: Alignment.bottomCenter,
+                  height: 500,
+                  child: Column(children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage()),
+                          );
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            Color.fromRGBO(153, 192, 124, 1),
+                          ),
+                          fixedSize: MaterialStateProperty.all<Size>(
+                            const Size(200, 60),
+                          ),
+                        ),
+                        child: Text(
+                          "Aluno",
+                          style: TextStyle(color: Colors.black),
+                        )),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromRGBO(153, 192, 124, 1),
+                        ),
+                        fixedSize: MaterialStateProperty.all<Size>(
+                          const Size(200, 60),
+                        ),
+                      ),
+                      child: const Text(
+                        "Professor",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromRGBO(153, 192, 124, 1),
+                        ),
+                        fixedSize: MaterialStateProperty.all<Size>(
+                          const Size(200, 60),
+                        ),
+                      ),
+                      child: const Text(
+                        "Coordenacao",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ])))
+        ],
+      ),
+    ));
+  }
+}
