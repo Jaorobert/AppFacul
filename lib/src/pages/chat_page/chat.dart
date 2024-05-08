ff8d57ac0576baf19ceffe93ab90362e67333e2a
Jaorobert <jaorobert125@gmail.com>
2024-05-07 22:10:59 -0300

    api de login e alteração em alguns layouts

--- lib/src/pages/chat_page/chat.dart
+++ lib/src/pages/chat_page/chat.dart
@@ -21,23 +21,22 @@ class MyApp extends StatelessWidget {
 class MessageListScreen extends StatelessWidget {
   final List<Message> messages = [
     Message(
-      text: 'Olá! Como você está?',
+      text: 'jao lindao',
       dateTime: DateTime.now(),
     ),
     Message(
-      text: 'Estou bem, obrigado!',
+      text: 'jao cherosao',
       dateTime: DateTime.now().subtract(Duration(minutes: 30)),
     ),
     Message(
-      text: 'E você?',
+      text: 'jao gostosao',
       dateTime: DateTime.now().subtract(Duration(hours: 2)),
     ),
   ];
 
   final List<Color> bubbleColors = [
-    Colors.blue[100]!, // Azul claro
-    Colors.blue[200]!, // Azul médio
-    Colors.blue[300]!, // Azul escuro
+    Colors.blue[200]!,
+    Colors.blue[300]!,
   ];
 
   @override
@@ -45,10 +44,9 @@ class MessageListScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Mensagens'),
-        backgroundColor:
-            Colors.green, // Alterando a cor de fundo da AppBar para verde
+        backgroundColor: Color.fromRGBO(72, 92, 57, 1),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back), // Ícone de seta para retornar
+          icon: Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pop(context);
           },
@@ -58,17 +56,15 @@ class MessageListScreen extends StatelessWidget {
         itemCount: messages.length,
         itemBuilder: (context, index) {
           final message = messages[index];
-          final color = bubbleColors[index %
-              bubbleColors.length]; // Alternando entre as cores da lista
+          final color = bubbleColors[index % bubbleColors.length];
           return Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: 8.0), // Adicionando espaçamento horizontal
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
             child: Align(
-              alignment: Alignment.topLeft, // Alinhando as mensagens à esquerda
+              alignment: Alignment.topLeft,
               child: Container(
                 margin: EdgeInsets.symmetric(vertical: 4.0),
                 padding: EdgeInsets.all(8.0),
-                width: double.infinity, // Ocupando toda a largura disponível
+                width: double.infinity,
                 decoration: BoxDecoration(
                   color: color,
                   borderRadius: BorderRadius.circular(8.0),
