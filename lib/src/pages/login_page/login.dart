b40bcfbfa29ecd541345f8f93039c9118c229a82
Jaorobert <jaorobert125@gmail.com>
2024-06-13 22:17:18 -0300

    muita coisa

--- lib/src/pages/login_page/login.dart
+++ lib/src/pages/login_page/login.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ui';
 import 'package:app_facul/src/pages/semeters_page/semeter.dart';
 import 'package:flutter/material.dart';
 import 'package:app_facul/src/data/services/api_login.dart';
@@ -20,129 +21,149 @@ class _LoginState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    BuildContext? localContext = context;
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 40,
-            vertical: 60,
-          ),
-          child: ListView(
-            children: [
-              SizedBox(
-                width: 128,
-                height: 128,
-                child: Image.asset('assets/imgs/logo-unicv.png'),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/imgs/fachada.jpg'),
+                fit: BoxFit.cover,
               ),
-              const SizedBox(height: 20),
-              Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      controller: controleEmail,
-                      decoration: const InputDecoration(
-                        labelText: 'RA ou E-mail',
-                        labelStyle: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 20,
-                        ),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Campo obrigatório';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      controller: controleSenha,
-                      decoration: const InputDecoration(
-                        labelText: 'Senha:',
-                        labelStyle: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 20,
-                        ),
-                      ),
-                      obscureText: true,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Campo obrigatório';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () async {
-                        if (formKey.currentState!.validate()) {
-                          formKey.currentState!.save();
-
-                          try {
-                            Response loginSuccess =
-                                await LoginServices.enviarAluno({
-                              'email': controleEmail.text,
-                              'password': controleSenha.text,
-                            });
-
-                            final Map<String, dynamic> jsonLoginResponse =
-                                json.decode(loginSuccess.body);
-
-                            if (jsonLoginResponse['bo_login']) {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => SemeterPage()));
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text(
-                                      'Falha no login! Login e/ou Senha incorretos!'),
-                                  backgroundColor: Colors.red,
-                                ),
-                              );
+            ),
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+            child: Container(
+              color: Colors.black.withOpacity(0.3),
+            ),
+          ),
+          Center(
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 40,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.8),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  SizedBox(
+                    width: 128,
+                    height: 128,
+                    child: Image.asset('assets/imgs/logo-unicc.png'),
+                  ),
+                  const SizedBox(height: 20),
+                  Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: controleEmail,
+                          decoration: const InputDecoration(
+                            labelText: 'RA ou E-mail',
+                            labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 20,
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Campo obrigatório';
                             }
-                          } catch (error) {
-                            print(error);
-                          }
-                        }
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color.fromRGBO(72, 92, 57, 1),
+                            return null;
+                          },
                         ),
-                        fixedSize: MaterialStateProperty.all<Size>(
-                          const Size(150, 35),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: controleSenha,
+                          decoration: const InputDecoration(
+                            labelText: 'Senha:',
+                            labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 20,
+                            ),
+                          ),
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Campo obrigatório';
+                            }
+                            return null;
+                          },
                         ),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () async {
+                            if (formKey.currentState!.validate()) {
+                              formKey.currentState!.save();
+
+                              try {
+                                Response loginSuccess =
+                                    await LoginServices.enviarAluno({
+                                  'email': controleEmail.text,
+                                  'password': controleSenha.text,
+                                });
+
+                                final Map<String, dynamic> jsonLoginResponse =
+                                    json.decode(loginSuccess.body);
+
+                                if (jsonLoginResponse['bo_login']) {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SemeterPage()));
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text(
+                                          'Falha no login! Login e/ou Senha incorretos!'),
+                                      backgroundColor: Colors.red,
+                                    ),
+                                  );
+                                }
+                              } catch (error) {
+                                print(error);
+                              }
+                            }
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color.fromRGBO(72, 92, 57, 1),
+                            ),
+                            fixedSize: MaterialStateProperty.all<Size>(
+                              const Size(150, 35),
+                            ),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(color: Colors.white),
                           ),
                         ),
-                      ),
-                      child: const Text(
-                        'Entrar',
-                        style: TextStyle(color: Colors.white),
-                      ),
+                        const SizedBox(height: 10),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text('Esqueci a senha.'),
+                        ),
+                      ],
                     ),
-                    const SizedBox(height: 10),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text('Esqueci a senha.'),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
